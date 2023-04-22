ec03485cf3bca31931c7ac1cc8163c0a6979dc2d
Joshua Ayeni <joshua@macs-MacBook-Air.local>
2023-04-22 01:49:27 +0100

    added referral bonus seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,5 +22,6 @@ class DatabaseSeeder extends Seeder
         (new ProductServiceTableSeeder)->run();
         (new ProductTableSeeder)->run();
         (new SingleUserSeeder)->run();
+        (new ReferralBonusSettingSeeder)->run();
     }
 }
