0419b0496df2d0e58da0efa22b03ce6bad50287b
Joshua Ayeni <joshua@macs-MacBook-Air.local>
2023-04-30 23:14:29 +0100

    Added email testing routes

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,50 @@ Route::get('/', function () {
     $data = Setting::find(1);
     return view('welcome',['data'=>$data]);
 });
+
+Route::get('/invite-guest-mail', function () {
+    $data = [
+        'sender_name'=>'Josh larry',
+        'sender_username'=>'josh',
+        'referrer'=>'simon'
+    ];
+ 
+    return new App\Mail\InviteGuestMail($data);
+});
+
+Route::get('/password-reset-mail', function () {
+    $data = '1234';
+    return new App\Mail\PasswordResetEmail($data);
+});
+
+Route::get('/registration-mail', function () {
+    $data = [
+        'name'=>'Josh larry',
+        'message'=>'A successful registration',
+    ];
+ 
+    return new App\Mail\RegistrationMail($data);
+});
+
+Route::get('/send-message-mail', function () {
+    $data = [
+        'subject'=>'Hey Josh',
+        'body'=>'Please pay up your money',
+    ];
+ 
+    return new App\Mail\InviteGuestMail($data);
+});
+
+Route::get('/two-factor-mail', function () {
+    $data = '1234';
+ 
+    return new App\Mail\TwoFactorVerifyMail($data);
+});
+
+Route::get('/confirmation-mail', function () {
+    $data = ['verification_code'=>'1234'];
+ 
+    return new App\Mail\UserConfirmationEmail($data);
+});
+
+
